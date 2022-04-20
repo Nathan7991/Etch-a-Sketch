5a98d151b625d4bf1f3424cc0067136e4da8216b
nathan <codejourney2022@gmail.com>
2022-04-20 15:16:45 -0400

    Add variables and while... loop statement

--- script.js
+++ script.js
@@ -3,4 +3,23 @@
 Variables
 =========
 */
-const grid = document.getElementsByClassName('grid');
+const grid = document.querySelector('.grid');
+
+let squareAmount = 16;
+
+let i = 0;
+
+let squares = document.createElement('div');
+
+while (i < squareAmount) {
+    let newSquare = document.createElement('div');
+    newSquare.setAttribute('class', 'squares');
+    grid.appendChild(newSquare);
+    i++
+}
+
+/*
+=========
+Functions
+=========
+*/
