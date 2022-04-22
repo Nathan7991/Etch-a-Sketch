3722d2e3e9192f48711d7a4f6579b1cdbf941c4a
nathan <codejourney2022@gmail.com>
2022-04-21 20:53:53 -0400

    Make changes to files
    Adjust the forEach method to create a squareContainer variable that holds the containers for squares. Create a for loop within the method that will determine if the initializer variable holds an integer that can be divided by 16 without a remainder. If so, then the squareContainer will be appended to the grid with a class attached to it as well.
    
    Attempt at nesting a for loop within the loop as well.
    
    Create .squareContainer class in style.css and set it's display property to flex

--- script.js
+++ script.js
@@ -8,20 +8,50 @@ let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 let result = '';
 
+let result2 = '';
+
 let i = 0;
 
 let divArray = [];
 
+let containerArray = [];
+
 while (i < squareAmount * squareAmount) {
     generateString();
+    generateString2();
     divArray.push(result);
+    containerArray.push(result2);
     i++;
 }
 
+/*
+containerArray.forEach(function(el) {
+    let squareContainer = document.createElement('div');
+    squareContainer.setAttribute('class', 'squareContainer');
+    grid.appendChild(squareContainer);
+});
+*/
+
 divArray.forEach(function(el) {
+    let div = document.createElement('div');
+    let squareContainer = document.createElement('div');
+
+    for (let j = 0; j < divArray.length; j++) {
+        if (j % squareAmount === 0) {
+            grid.appendChild(squareContainer);
+            squareContainer.setAttribute('class', 'squareContainer');
+            for (let x = 0; x < divArray.length; x++) {
+                div.setAttribute('class', 'squares');
+                squareContainer.appendChild(div);
+            }
+        }
+    }
+    
+    /*
     let div = document.createElement('div');
     div.setAttribute('class', 'squares');
     grid.appendChild(div);
+    */
 });
 
 
@@ -43,4 +73,13 @@ function generateString(length = 1) {
     }
 
     return result;
+}
+
+function generateString2(length = 1) {
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result2 += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result2;
 }
